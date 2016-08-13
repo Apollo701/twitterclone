26369b486611bf35f7c4fe7be639c831fd3c0755
Jason Dorn <apollo701@gmail.com>
2016-08-13 16:46:01 -0700

    migrated tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813233911) do
+ActiveRecord::Schema.define(version: 20160813234219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20160813233911) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "tweet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
+  factory :tweet do
+    user_id 1
+    tweet "MyString"
+  end
   factory :user do
     username "MyString"
     email "MyString"
